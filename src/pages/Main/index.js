ce1a4d31da8bd0b16495c47d1652e36e10968eb8
Christian Fleury <chfleurysiq@gmail.com>
2021-01-11 10:56:28 -0300

    first commit

---
+++ src/pages/Main/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import { Container, Form, SubmitButton } from './styles';
+
+function Main() {
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input placeholder="Adicionar repositório" type="text" />
+
+        <SubmitButton disabled>
+          <FaPlus color="#FFF" size="14" />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
+}
+
+export default Main;
---
+++ src/pages/Main/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  color: #fff;
+`;
+
+export const Container = styled.div`
+  color: #fff;
+`;
+
+export const Form = styled.form``;
+
+export const SubmitButton = styled.button`
+  color: #fff;
+`;
